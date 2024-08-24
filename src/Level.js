ceed568e66d9c2fe16a7f0d5d9b488a20bc7e7e9
alessandrobattiato <alessandroDev@outlook.it>
2024-08-24 23:48:18 +0200

    feat: add position prop and wrap block start content into group tag in preparation of future additional blocks to be rendered

--- src/Level.js
+++ src/Level.js
@@ -1,18 +1,20 @@
 import React from "react";
 
-const BlockStart = () => {
+const BlockStart = ({ position = [0, 0, 0] }) => {
     return (
-        <mesh position={[0, -0.1, 0]} receiveShadow>
-            <boxGeometry args={[4, 0.2, 4]} />
-            <meshStandardMaterial color="limegreen" />
-        </mesh>
+        <group position={position}>
+            <mesh position={[0, -0.1, 0]} receiveShadow>
+                <boxGeometry args={[4, 0.2, 4]} />
+                <meshStandardMaterial color="limegreen" />
+            </mesh>
+        </group>
     );
 };
 
 const Level = () => {
     return (
         <>
-            <BlockStart />
+            <BlockStart position={[0, 0, 0]} />
         </>
     );
 };
