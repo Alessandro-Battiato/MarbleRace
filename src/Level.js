6e9a0fa57b4d4f54e24e134e9ea957dac66eb511
alessandrobattiato <alessandroDev@outlook.it>
2024-08-25 00:46:04 +0200

    feat: add axe obstacle

--- src/Level.js
+++ src/Level.js
@@ -115,12 +115,58 @@ const BlockLimbo = ({ position = [0, 0, 0] }) => {
     );
 };
 
+const BlockAxe = ({ position = [0, 0, 0] }) => {
+    const obstacleRef = useRef(null);
+    const [timeOffset] = useState(() => Math.random() * Math.PI * 2);
+
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime();
+
+        const x = Math.sin(time + timeOffset) * 1.25;
+        obstacleRef.current.setNextKinematicTranslation({
+            x: position[0] + x,
+            y: position[1] + 0.75,
+            z: position[2],
+        });
+    });
+
+    return (
+        <group position={position}>
+            {/* Floor */}
+            <mesh
+                geometry={boxGeometry}
+                material={floor2Material}
+                position={[0, -0.1, 0]}
+                scale={[4, 0.2, 4]}
+                receiveShadow
+            />
+            {/* Obstacle */}
+            <RigidBody
+                ref={obstacleRef}
+                type="kinematicPosition"
+                position={[0, 0.3, 0]}
+                restitution={0.2}
+                friction={0} // we set 0 so that the ball just needs to slightly bounce, the player does not have to get stuck
+            >
+                <mesh
+                    geometry={boxGeometry}
+                    material={obstacleMaterial}
+                    scale={[1.5, 1.5, 0.3]}
+                    castShadow
+                    receiveShadow
+                />
+            </RigidBody>
+        </group>
+    );
+};
+
 const Level = () => {
     return (
         <>
-            <BlockStart position={[0, 0, 4]} />
-            <BlockSpinner position={[0, 0, 0]} />
+            <BlockStart position={[0, 0, 12]} />
+            <BlockSpinner position={[0, 0, 8]} />
             <BlockLimbo position={[0, 0, 4]} />
+            <BlockAxe position={[0, 0, 0]} />
         </>
     );
 };
