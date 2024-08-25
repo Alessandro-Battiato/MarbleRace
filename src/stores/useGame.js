2fb38ec0df486cc78caa094d9bed8dd1645568c9
alessandrobattiato <alessandroDev@outlook.it>
2024-08-25 15:12:22 +0200

    feat: create phase methods

--- src/stores/useGame.js
+++ src/stores/useGame.js
@@ -1,7 +1,27 @@
 import { create } from "zustand";
 
-export default create(() => {
+export default create((set) => {
     return {
         blocksCount: 3,
+        /**
+         * Phases
+         */
+        phase: "ready",
+        start: () => {
+            set(() => {
+                // The set function returns the new, updated, state, useful for us so that we can update the phase
+                return { phase: "playing" };
+            });
+        },
+        restart: () => {
+            set(() => {
+                return { phase: "ready" };
+            });
+        },
+        end: () => {
+            set(() => {
+                return { phase: "ended" };
+            });
+        },
     };
 });
