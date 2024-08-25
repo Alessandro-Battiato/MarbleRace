f6a169e6be41e45638f39a4c056daac111623555
alessandrobattiato <alessandroDev@outlook.it>
2024-08-25 15:08:30 +0200

    feat: create store and blocks count global value to set the level length

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,15 +3,19 @@ import Lights from "./Lights.jsx";
 import { Level } from "./Level.jsx";
 import { Physics } from "@react-three/rapier";
 import Player from "./Player.jsx";
+import useGame from "./stores/useGame.js";
 
 export default function Experience() {
+    // It's best to only select the props you need to use in the component because each change re-renders the component
+    const blocksCount = useGame((state) => state.blocksCount); // callback selector for the blocks count
+
     return (
         <>
             <OrbitControls makeDefault />
 
             <Physics>
                 <Lights />
-                <Level />
+                <Level count={blocksCount} />
                 <Player />
             </Physics>
         </>
