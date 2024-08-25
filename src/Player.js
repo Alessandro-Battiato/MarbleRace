17435eaff6d4d50f782f241365cf6a004504f815
alessandrobattiato <alessandroDev@outlook.it>
2024-08-25 12:50:35 +0200

    feat: enable player controls

--- src/Player.js
+++ src/Player.js
@@ -1,20 +1,61 @@
-import React from "react";
+import React, { useRef } from "react";
 import { RigidBody } from "@react-three/rapier";
+import { useFrame } from "@react-three/fiber";
+import { useKeyboardControls } from "@react-three/drei";
 
 const Player = () => {
-    // We are only casting a shadow and not receiving it for gameplay purposes, so we can still see the ball, even if it looks less realistic
-    // Flatshading is added so the player can "see" the rotation happening
+    const [subscribeKeys, getKeys] = useKeyboardControls(); // The 2 destructured functions: subscribeKeys is used to subscribe to key changes (useful to know when the jump key has been pressed), getKeys is a function used to get the current states of the keys (useful to know if the WASD keys are being pressed)
+    const marbleRef = useRef();
+
+    useFrame((state, delta) => {
+        const { forward, backward, leftward, rightward } = getKeys();
+
+        // We set 1 impulse and 1 torque with these values because sometimes the player could press 2 buttons while airborne like forward and then right for example and this makes it work right
+        const impulse = { x: 0, y: 0, z: 0 };
+        const torque = { x: 0, y: 0, z: 0 };
+
+        // We use delta (time passed between each frame) in order to make the strength of the controls the same for all of the players, otherwise players with higher framerate would have a "stronger" ball movement
+        const impulseStrength = 0.6 * delta;
+        const torqueStrength = 0.2 * delta;
+
+        if (forward) {
+            impulse.z -= impulseStrength; // To move towards the burger, the end of the level, the ball has to move on the z axis
+            torque.x -= torqueStrength; // To move sideways, it moves on the x axis
+        }
+        if (rightward) {
+            impulse.x += impulseStrength;
+            torque.z -= torqueStrength;
+        }
+        if (backward) {
+            impulse.z += impulseStrength;
+            torque.x += torqueStrength;
+        }
+        if (leftward) {
+            impulse.x -= impulseStrength;
+            torque.z += torqueStrength;
+        }
+
+        marbleRef.current.applyImpulse(impulse);
+        marbleRef.current.applyTorqueImpulse(torque);
+    });
+
     return (
         <RigidBody
+            ref={marbleRef}
             canSleep={false} // In the latest version of @react-three/rapier and rapier, a RigidBody falls asleep after a few seconds of inaction. This would result in the sphere not moving even though the player presses the arrow keys. The prop can sleep set to false fixes the issue
             colliders="ball"
             restitution="0.2"
             friction={1} // Same reason explained in the Level file
             position={[0, 1, 0]}
         >
-            <mesh castShadow>
+            <mesh
+                castShadow // We are only casting a shadow and not receiving it for gameplay purposes, so we can still see the ball, even if it looks less realistic
+            >
                 <icosahedronGeometry args={[0.3, 1]} />
-                <meshStandardMaterial flatShading color="mediumpurple" />
+                <meshStandardMaterial
+                    flatShading // Flatshading is added so the player can "see" the rotation happening
+                    color="mediumpurple"
+                />
             </mesh>
         </RigidBody>
     );
--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,31 @@ import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
+import { KeyboardControls } from "@react-three/drei";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-    <Canvas
-        shadows
-        camera={{
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [2.5, 4, 6],
-        }}
+    <KeyboardControls
+        // We use KeyW, KeyS ecc, to cover the non-QWERTY keyboards as well, setting only w or s wouldn't work for these keyboards
+        map={[
+            { name: "forward", keys: ["ArrowUp", "KeyW"] },
+            { name: "backward", keys: ["ArrowDown", "KeyS"] },
+            { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+            { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+            { name: "jump", keys: ["Space"] },
+        ]} // In the map array, we need to provide each key that we want to observe as an object with a name and the list of keys that should trigger the change as another array.
     >
-        <Experience />
-    </Canvas>
+        <Canvas
+            shadows
+            camera={{
+                fov: 45,
+                near: 0.1,
+                far: 200,
+                position: [2.5, 4, 6],
+            }}
+        >
+            <Experience />
+        </Canvas>
+    </KeyboardControls>
 );
